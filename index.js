6fc10554f470c4b4a25e7d14a24b4cb59ddb002f
souhype <souhype@protonmail.com>
2022-10-30 15:23:12 +0100

    .

--- index.js
+++ index.js
@@ -11,65 +11,86 @@ class Model {
 
     init() {
         this.scene = new THREE.Scene();
-        // this.scene.background = new THREE.Color('#1a1a1a');
 
-        this.camera = new THREE.PerspectiveCamera(45, innerWidth / innerHeight, 1, 1000);
-        this.camera.position.set(0, 100, 350);
-        // this.camera.lookAt(0, 10, 0);
+        this.aspectRatio = innerWidth / innerHeight;
+        this.camera = new THREE.PerspectiveCamera(45, this.aspectRatio, 1, 1000);
+        this.camera.position.set(0, 125, 350);
+        this.camera.lookAt(0, 100, 0);
 
         this.renderer = new THREE.WebGLRenderer({
             antialias: true,
             canvas: document.querySelector('canvas'),
         });
-        this.renderer.setSize(innerWidth * 0.95, innerHeight * 0.95);
-        // this.renderer.shadowMap.enabled = true;
+        this.renderer.setSize(innerWidth, innerHeight * 0.95);
+        this.renderer.setPixelRatio(devicePixelRatio);
+        this.renderer.shadowMap.enabled = true;
 
+        this.clock = new THREE.Clock();
+
+        this.enviroment();
+        this.loader();
+        this.animate();
+    }
+
+    enviroment() {
         const hemiLight = new THREE.HemisphereLight();
         const directLight = new THREE.DirectionalLight();
-        // directLight.castShadow = true;
-        // directLight.shadow.camera.top = 200;
-        // directLight.shadow.camera.bottom = -100;
-        // directLight.shadow.camera.left = -120;
-        // directLight.shadow.camera.right = 120;
+        directLight.position.set(-150, 400, 200);
+        directLight.castShadow = true;
+        directLight.shadow.camera.top = 200;
+        directLight.shadow.camera.bottom = -200;
+        directLight.shadow.camera.left = -200;
+        directLight.shadow.camera.right = 200;
 
         const wall = new THREE.Mesh(
             new THREE.PlaneGeometry(900, 900),
             new THREE.MeshPhongMaterial({ color: 'white', depthWrite: false })
         );
+        wall.receiveShadow = true;
 
         const ground = new THREE.Mesh(
             new THREE.PlaneGeometry(900, 900),
             new THREE.MeshPhongMaterial({ color: '#808080', depthWrite: false })
         );
         ground.rotation.x = -Math.PI * 0.5;
-        // ground.receiveShadow = true;
+        ground.receiveShadow = true;
 
         const groundGrid = new THREE.GridHelper(1500, 20, 'black', 'black');
-        groundGrid.material.opacity = 0.1;
+        groundGrid.material.opacity = 0.2;
         groundGrid.material.transparent = true;
 
         const wallGrid = new THREE.GridHelper(700, 20, 'black', 'black');
-        wallGrid.material.opacity = 0.1;
+        wallGrid.material.opacity = 0.2;
         wallGrid.material.transparent = true;
         wallGrid.rotation.x = -Math.PI * 0.5;
-        wallGrid.position.set(0, 414, -10);
+        wallGrid.position.set(0, 430, -10);
 
-        this.scene.add(groundGrid, wallGrid, hemiLight, directLight, ground, wall);
-
-        this.animate();
-        this.loader();
+        this.scene.add(hemiLight, directLight, ground, wall, groundGrid, wallGrid);
     }
 
     loader() {
         const fbxLoader = new FBXLoader();
-        fbxLoader.load('./assets/T_Pose_Bot.fbx', (object) => {
-            // object.scale.setScalar(0.1);
+        fbxLoader.load('./assets/Running.fbx', (object) => {
+            object.quaternion['_y'] = 0.7;
+            object.position['x'] = -150;
+            this.mixer = new THREE.AnimationMixer(object);
+            const run = this.mixer.clipAction(object.animations[0]);
+            run.play();
+
+            object.traverse((child) => {
+                if (child.isMesh) {
+                    child.castShadow = true;
+                    child.receiveShadow = true;
+                }
+            });
             this.scene.add(object);
         });
     }
 
     animate = () => {
         requestAnimationFrame(this.animate);
+        const delta = this.clock.getDelta();
+        if (this.mixer) this.mixer.update(delta);
         this.renderer.render(this.scene, this.camera);
     };
 }
